2262c933e2a7f4455a5a1b309ba38af5b001d16c
albamc <albamc@gmail.com>
2017-04-11 13:55:49 +0900

    Update constants.go

--- constants.go
+++ constants.go
@@ -1,11 +1,11 @@
 package shipdock
 
 const (
-  HTTPHEADERPREFIX = "com.navercorp."
-  CLUSTER          = "com.navercorp.cluster.name"
-  RACK             = "com.navercorp.rack.name"
-  OWNER            = "com.docker.swarm.owner"
+  HTTPHEADERPREFIX     = "com.navercorp."
+  CLUSTER              = "com.navercorp.cluster.name"
+  RACK                 = "com.navercorp.rack.name"
+  OWNER                = "com.docker.swarm.owner"
   
-  ENABLE_ALLOC_VIP = "com.navercorp.shipdock.allocvip"
-  KVSTORE          = "com.navercorp.shipdock.kvstore"
+  ENV_ENABLE_ALLOC_VIP = "ENABLE_ALLOC_VIP"
+  ENV_KVSTORE_URL      = "KVSTORE_URL"
 )
