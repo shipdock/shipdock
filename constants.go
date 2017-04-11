b4a7279e9c9c90d03866139b4722897e2043bcff
albamc <albamc@gmail.com>
2017-04-11 14:02:26 +0900

    Update constants.go

--- constants.go
+++ constants.go
@@ -5,9 +5,11 @@ const (
   CLUSTER              = "com.navercorp.cluster.name"
   RACK                 = "com.navercorp.rack.name"
   OWNER                = "com.docker.swarm.owner"
+  SERVICE_IP           = "com.navercorp.shipdock.lb.service_ip"
+  SERVICE_PORTS        = "com.navercorp.shipdock.lb.service_ports"
   
   ENV_ENABLE_ALLOC_VIP = "ENABLE_ALLOC_VIP"
-  ENV_KVSTORE_URL      = "KVSTORE_URL"
-  ENV_CLUSTER_NAME     = "CLUSTER_NAME"
-  ENV_RACK_NAME        = "RACK_NAME"
+  ENV_KVSTORE          = "KVSTORE"
+  ENV_CLUSTER          = "CLUSTER"
+  ENV_RACK             = "RACK"
 )
