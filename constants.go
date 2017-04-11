084b5b31a7425fec0af5c24414abda3307ba2e67
albamc <albamc@gmail.com>
2017-04-11 10:09:32 +0900

    Create constants.go

---
+++ constants.go
@@ -0,0 +1,8 @@
+package shipdock
+
+const (
+  HTTPHEADERPREFIX = "com.navercorp."
+  CLUSTER          = "com.navercorp.cluster.name"
+  RACK             = "com.navercorp.rack.name"
+  OWNER            = "com.docker.swarm.owner"
+)
